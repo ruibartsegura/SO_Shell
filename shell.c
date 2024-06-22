b02fdcc5a0825aca3fafeebd890a68db13f91513
r.bartolome.2022 <r.bartolome.2022@alumnos.urjc.es>
2024-06-22 14:29:42 +0200

    Shell actualizada

--- shell.c
+++ shell.c
@@ -7,6 +7,7 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <signal.h>
 
 enum {
 	MAX_LONG = 1024,
@@ -17,18 +18,127 @@ enum {
 void
 ejecutar_cmd(char *cmd, char **args)
 {
-	execv(cmd, args);	// Ejecutar el comando ping
+	execv(cmd, args);	// Ejecutar el comando
 	// Si execv() devuelve, ha habido un error
 	perror("execv");
 	exit(EXIT_FAILURE);
 }
 
 void
+do_cd(char **cmd)
+{
+	if (cmd[1] != NULL) {
+		if (chdir(cmd[1]) == -1)
+			perror("cd");
+	} else {
+		cmd[1] = getenv("HOME");
+		if (chdir(cmd[1]) == -1)
+			perror("cd");
+	}
+}
+
+int
+asignar_var(char *entrada)
+{
+	char *valor;
+	char *igual;
+
+	igual = strchr(entrada, '=');
+	if (igual != NULL && ((igual - entrada[0]) != 0)) {
+		// Cambiar el = por /0 para separar la variable de su valor
+		*igual = '\0';
+		valor = igual + 1;
+
+		setenv(entrada, valor, 1);
+
+		return 0;
+	}
+	return -1;
+}
+
+char *
+sustituir_var(char *var)
+{
+	char *aux;
+	char *sust;
+
+	aux = strchr(var, '$');
+	if (aux != NULL) {
+		*aux = '\0';
+		sust = aux + 1;
+	}
+	return getenv(sust);
+
+}
+
+char *
+buscar_cmd(char *cmd)
+{
+	char *path;
+	char *final;
+	char path_aux[MAX_LONG];
+
+	path = getenv("PATH");
+
+	char **token = malloc(MAX_LONG * sizeof(char *));
+
+	memset(token, 0, MAX_LONG * sizeof(char *));
+
+	if (!token) {
+		fprintf(stderr, "Error al asignar memoria\n");
+		exit(EXIT_FAILURE);
+	}
+	char *delim = ":";
+	char *saveptr;
+
+	int position = 0;
+
+	// Primera llamada a strtok_r con la cadena original
+	token[position] = strtok_r(path, delim, &saveptr);
+	while (token[position] != NULL) {
+		if (position >= MAX_LONG) {
+			fprintf(stderr, "Demasiados argumentos\n");
+			exit(EXIT_FAILURE);
+		}
+		position++;
+
+		// Llamadas subsecuentes a strtok_r con NULL
+		token[position] = strtok_r(NULL, delim, &saveptr);
+	}
+
+	position = 0;
+	while (token[position] != NULL) {
+
+		// General la direccion del fichero para comprobar si es ejecutable
+		strcpy(path_aux, token[position]);
+		strcat(path_aux, "/");
+		strcat(path_aux, cmd);
+
+		if (access(path_aux, X_OK) == 0) {
+			return final = path_aux;
+		}
+		position++;
+	}
+
+	// Comprobar en el directorio actual
+	getcwd(path_aux, sizeof(path_aux));
+	strcat(path_aux, "/");
+	strcat(path_aux, cmd);
+
+	if (access(path_aux, X_OK) == 0) {
+		return final = path_aux;
+	}
+
+	return NULL;
+}
+
+void
 redir_cmd(char **comando, int in_fd, int out_fd)
 {
-	char accion[MAX_LONG] = "/bin/";
+	char *accion;
+	int pid;
 
-	switch (fork()) {
+	switch (pid = fork()) {
 	case -1:
 		err(EXIT_FAILURE, "cannot fork");
 	case 0:
@@ -50,16 +160,18 @@ redir_cmd(char **comando, int in_fd, int out_fd)
 			close(out_fd);
 		}
 		// Ejecutar cmd
-		strcat(accion, comando[0]);
-		ejecutar_cmd(accion, comando);
+		if ((accion = buscar_cmd(comando[0])) != NULL)
+			ejecutar_cmd(accion, comando);
+		else {
+			fprintf(stderr, "Comando %s no válido.\n", comando[0]);
+			exit(EXIT_FAILURE);
+		}
 	}
 }
 
 void
 preparar_cmd(char **comando, int pipes, int redir_in, int redir_out, int shh)
 {
-	//printf("pipes %d, redir %d, shh %d\n", pipes, redir, shh);
-
 	if (pipes == 0 && redir_in == 0 && redir_out == 0) {
 		int in_fd = STD_IO, out_fd = STD_IO;
 
@@ -275,7 +387,8 @@ preparar_cmd(char **comando, int pipes, int redir_in, int redir_out, int shh)
 		} else {
 			out_fd = STD_IO;
 		}
-		redir_cmd(cmd, in_fd, out_fd);	// Ejecutar el último comando sin redirección de salida
+
+		redir_cmd(cmd, in_fd, out_fd);
 
 		free(cmd);
 		close(fd[0]);
@@ -331,6 +444,18 @@ leer_entrada(char **entrada)
 	redir_out = 0;
 	shh = 0;
 
+	// Comprobar si es el bultin cd
+	if (strcmp(entrada[0], "cd") == 0 && entrada[2] == NULL) {
+		do_cd(entrada);
+		return;
+	} else if (strcmp(entrada[0], "cd") == 0 && entrada[2] != NULL) {
+		fprintf(stderr, "Demasiados argumentos\n");
+		return;
+	}
+	// Comprobar si hay una asignación de variable y hacerla en caso de que si
+	if (asignar_var(entrada[0]) == 0)
+		return;
+
 	for (int i = 0; entrada[i] != NULL; i++) {
 		if (strcmp(entrada[i], "|") == 0 && redir_in == 0
 		    && redir_out == 0 && shh == 0) {
@@ -360,6 +485,9 @@ leer_entrada(char **entrada)
 			fprintf(stderr, "Sintaxis del comando erronea\n");
 			return;
 		}
+		if (entrada[i][0] == '$') {
+			entrada[i] = sustituir_var(entrada[i]);
+		}
 	}
 	preparar_cmd(entrada, pipes, redir_in, redir_out, shh);
 }
@@ -369,6 +497,7 @@ main(int argc, char *argv[])
 {
 	argc--;
 	char entrada[MAX_LONG];
+	char path[MAX_LONG];
 	char **token;
 
 	if (argc != 0) {
@@ -376,12 +505,15 @@ main(int argc, char *argv[])
 		     "Argumentos esperados 0, argumentos recibidos %d\n", argc);
 	}
 
+	fprintf(stderr, "%s$ ", getcwd(path, sizeof(path)));
+
 	while (fgets(entrada, MAX_LONG, stdin) != NULL) {
 		token = extraer_tokens(entrada);
 
 		leer_entrada(token);
 
 		free(token);
+		fprintf(stderr, "\n%s$ ", getcwd(path, sizeof(path)));
 	}
 	exit(EXIT_SUCCESS);
 }
